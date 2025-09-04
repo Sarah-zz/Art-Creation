08f7018bbec7e63e84a8dd5aa7c9453f6897e99c
Sarah-zz <sarah.zogbi02@gmail.com>
2025-09-04 10:29:01 +0200

    feat : affichage galery via le back

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -2,21 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\GalleryRepository;
 use App\Database\MongoDbConnection;
 
 class GalleryController
 {
+    private GalleryRepository $repository;
+
+    public function __construct()
+    {
+        $this->repository = new GalleryRepository();
+    }
+
     public function index(): array
     {
-        $images = [
-            ['id' => 1, 'url' => 'https://cdn.pixabay.com/photo/2021/06/02/21/37/abstract-6305508_1280.jpg', 'title' => 'Red'],
-            ['id' => 2, 'url' => 'https://cdn.pixabay.com/photo/2021/08/18/19/20/painting-6556384_1280.jpg', 'title' => 'Lac rose'],
-            ['id' => 3, 'url' => 'https://cdn.pixabay.com/photo/2018/08/19/07/05/background-3616101_1280.jpg', 'title' => 'La Plage'],
-            ['id' => 4, 'url' => 'https://cdn.pixabay.com/photo/2022/09/18/23/54/black-woman-7464238_1280.jpg', 'title' => 'Vent'],
-            ['id' => 5, 'url' => 'https://cdn.pixabay.com/photo/2021/08/18/19/26/background-6556413_1280.jpg', 'title' => 'Mer'],
-            ['id' => 6, 'url' => 'https://media.istockphoto.com/id/485991870/fr/vectoriel/jus-dorange.jpg?s=612x612&w=0&k=20&c=Dxf6-v2chZJtKGALkJEd9gD1YBY5Es25iv_yk5Udvzs=', 'title' => 'Feu'],
-        ];
-
+        // Récupère toutes les images depuis MySQL
+        $images = $this->repository->findAll();
         return ['images' => $images];
     }
 
--- src/View/gallery.php
+++ src/View/gallery.php
@@ -10,10 +10,12 @@ $images = $data['images'] ?? [];
             <?php foreach ($images as $img): ?>
                 <div class="col-md-4 d-flex">
                     <div class="card gallery-card img-clickable h-100 w-100" data-title="<?= htmlspecialchars($img['title']) ?>"
-                        data-size="1024x768" data-description="Description du tableau en cours.">
+                        data-size="<?= htmlspecialchars($img['size'] ?? '') ?>"
+                        data-description="<?= htmlspecialchars($img['description'] ?? '') ?>">
 
                         <div class="gallery-img-wrapper">
-                            <img src="<?= $img['url'] ?>" class="gallery-img" alt="<?= htmlspecialchars($img['title']) ?>">
+                            <img src="<?= htmlspecialchars($img['image']) ?>" class="gallery-img"
+                                alt="<?= htmlspecialchars($img['title']) ?>">
                         </div>
 
                         <div class="card-body text-center position-relative">
