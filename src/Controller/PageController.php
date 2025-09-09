17a44da666188f4d83fe58fef6157a6659178b53
Sarah-zz <sarah.zogbi02@gmail.com>
2025-09-09 11:41:50 +0200

    feat : correction redirection page

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -9,11 +9,16 @@ class PageController
     {
         $this->viewPath = dirname(__DIR__) . '/View';
     }
-
-    public function render(string $page): array
+    public function render(string $page = 'home'): array
     {
+        $file = $this->viewPath . '/' . $page . '.php';
+        if (!file_exists($file)) {
+            http_response_code(404);
+            $file = $this->viewPath . '/error404.php';
+        }
+
         return [
-            'view' => $this->viewPath . '/' . $page . '.php',
+            'view' => $file,
             'data' => []
         ];
     }
