a92cce9f6f0540927b427e0764614d1de0868d3e
Sarah-zz <sarah.zogbi02@gmail.com>
2025-09-27 22:16:47 +0200

    test hello world

--- public/index.php
+++ public/index.php
@@ -1,7 +1,2 @@
 <?php
-// Afficher toutes les erreurs pour le debug
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-echo "<h1>Art & Création - Test Index</h1>";
+echo "Hello World!";
